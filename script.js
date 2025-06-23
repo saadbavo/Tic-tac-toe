59634aa92a20e5ab759019b0e468374327687a2a
saadbavo <saadhmk13@gmail.com>
2025-06-24 00:08:10 +0100

    Add plyzr object with nam and mark

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ const Gamboard = ( ()=> {
     const resetBoard = () => {
         board.fill("");
     };
-    const placeMarker = (index, mark) => {
+    const placeMarker = (index, player) => {
         if (index < 0 || index >= board.length || board[index] !== "") {
             throw new Error("Invalid move");
         }
-        board[index] = mark;
+        board[index] = player.mark;
     }
-    const getBoard = () => board;
+    getBoard = () => board;
+    return { resetBoard, placeMarker, getBoard };
 })();
 
 const player = (name, mark) => {
     return { name, mark };
 }
+const player1 = player("Player 1", "X");
+const player2 = player("Player 2", "O");
