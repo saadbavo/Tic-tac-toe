65d8a07bf6d5e850fd7ca16510903ea087d79763
saadbavo <saadhmk13@gmail.com>
2025-06-26 23:29:33 +0100

    Add chech winner to check for win or draw

--- script.js
+++ script.js
@@ -16,6 +16,25 @@ const Gamboard = ( ()=> {
 
 const player = (name, mark) => {
     return { name, mark };
+};
+
+const checkWinner = (board) => {
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+        [0, 4, 8], [2, 4, 6] // diagonals
+    ];
+    
+    for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return board[a]; // Return the mark of the winner
+        }
+        else if (board.every(cell => cell !== "")) {
+        return "draw"; // Return "draw" if all cells are filled and no winner
+    }
+    }
+    return null; // No winner yet
+    
 }
-const player1 = player("Player 1", "X");
-const player2 = player("Player 2", "O");
+
